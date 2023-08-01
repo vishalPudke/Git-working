18d93f63f858a21d8f988ed574c6825685b2c3a4
vishalPudke <vishal.mechtech19@gmail.com>
2023-08-01 12:38:20 +0530

    thank you git

--- App.js
+++ App.js
@@ -1,2 +1,14 @@
 console.log("Vishal learning git");
 console.log("Merge practice");
+console.log("thank you git ");
+console.log("DVCS");
+console.log(
+  "because iof you we can easily track our project and ,maintained different version of project"
+);
+console.log("Thank you for your help");
+console.log(
+  "git commands are git init -->> git remote add orogin 'url of your repository' -->> git commit  -m 'message'-->> git push -u origin name of branch "
+);
+console.log(
+  "git checkout -b b1 -->> git merge main -->> git push -u origin main"
+);
